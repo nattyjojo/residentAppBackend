ed52f69ca8ffbcb13bfeb9aae576fce6507d0722
nattyjojo <nattyjojo9@gmail.com>
2023-12-14 11:58:39 +0100

    added files

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ import content from "./routes/post.js";
 import getUsers from "./routes/get-users.js";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://657ad870f0f6a8000860e6da--dainty-selkie-5509f1.netlify.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.static("userFiles"));
 app.use(cors(corsOptions));
 app.use(express.json());
