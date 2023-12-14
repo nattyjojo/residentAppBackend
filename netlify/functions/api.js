8293e63b00114411c7594a836f33d35b9b9405a6
nattyjojo <nattyjojo9@gmail.com>
2023-12-14 12:18:48 +0100

    added files

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
