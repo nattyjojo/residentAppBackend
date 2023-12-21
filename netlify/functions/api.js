670fdf8cd52437e4345b2714f433f6bc7d08d481
nattyjojo <nattyjojo9@gmail.com>
2023-12-21 20:06:35 +0100

    test

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -29,6 +29,10 @@ app.use(getUsers);
 app.use(getUserFiles);
 
 export const handler = serverless(app, {
-  binary: ["application/json", "image/png"],
+  binary: ["image/*"],
+
   //basePath: "https://residentapp.johnchimezie.online",
 });
+// https://residentapi.johnchimezie.online/files/userFiles/banner/ad56b096-5b9e-430c-a4b6-c544cbb23a7a-banner.png
+//https://residentapp.johnchimezie.online/_ipx/w_128,q_75/https%3A%2F%2Fresidentapi.johnchimezie.online%2Ffiles%2Fbanner%2Fad56b096-5b9e-430c-a4b6-c544cbb23a7a-banner.png?url=https%3A%2F%2Fresidentapi.johnchimezie.online%2Ffiles%2Fbanner%2Fad56b096-5b9e-430c-a4b6-c544cbb23a7a-banner.png&w=128&q=75
+//"application/json",
