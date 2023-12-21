4b26ee45414a216a02ebd8b469b017828a6a1852
nattyjojo <nattyjojo9@gmail.com>
2023-12-21 09:32:40 +0100

    test

--- routes/files.js
+++ routes/files.js
@@ -4,5 +4,5 @@ const banner = Router();
 banner.use("/files/:image", (req, res, next) => {
   next();
 });
-banner.use("/files/:image", express.static("userFiles"));
+banner.use("/files/:image", express.static("./userFiles"));
 export default banner;
