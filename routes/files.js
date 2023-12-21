470f0ff88aa98387795e4d96932c16d155dce0a9
nattyjojo <nattyjojo9@gmail.com>
2023-12-21 13:29:13 +0100

    test

--- routes/files.js
+++ routes/files.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import express from "express";
 const banner = Router();
-banner.use("/files/:image", (req, res, next) => {
-  next();
-});
-banner.use("/files/:image", express.static("userFiles/banner"));
+
+banner.use("/files", express.static("userFiles/banner"));
 export default banner;
