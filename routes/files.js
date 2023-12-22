8ebb119d6f454d3b31133540c50ca677388cfcbc
nattyjojo <nattyjojo9@gmail.com>
2023-12-22 10:00:47 +0100

    test

--- routes/files.js
+++ routes/files.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
 const banner = Router();
-
-banner.use("/files/:imagePath", express.static("netlify/functions/userFiles"));
+const __dirname = path.dirname(__filename) + "/userFiles";
+banner.use("/files/:imagePath", (req, res, next) => {
+  console.log(__dirname);
+  next();
+});
+banner.use("/files/:imagePath", express.static(__dirname));
 export default banner;
