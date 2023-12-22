b6bc6c4144a0a1b0274bc3bafbd5e95f88a89078
nattyjojo <nattyjojo9@gmail.com>
2023-12-22 09:46:19 +0100

    test

--- routes/files.js
+++ routes/files.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import express from "express";
 const banner = Router();
 
-banner.use("/files/:imagePath", express.static("userFiles"));
+banner.use("/files/:imagePath", express.static("netlify/functions/userFiles"));
 export default banner;
