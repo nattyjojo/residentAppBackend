31d45689964ee6e9c689fcdcf867a4a21656b6b7
nattyjojo <nattyjojo9@gmail.com>
2023-12-22 10:33:49 +0100

    test

--- routes/files.js
+++ routes/files.js
@@ -1,32 +1,7 @@
-// import { Router } from "express";
-// import express from "express";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// const __filename = fileURLToPath(import.meta.url);
-// const banner = Router();
-
-// banner.use(
-//   "/files/:imagePath",
-//   express.static(path.dirname(__filename) + "/userFiles")
-// );
-// export default banner;
-// banner.use("/files/:imagePath", (req, res, next) => {
-//     console.log(path.dirname(__filename) + "/userFiles");
-//     next();
-//   });
 import { Router } from "express";
 import express from "express";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const banner = Router();
 
-banner.use(
-  "/files/:imagePath",
-  express.static(path.join(__dirname, "userFiles"))
-);
-
+banner.use("/files/:imagePath", express.static("./userFiles"));
 export default banner;
